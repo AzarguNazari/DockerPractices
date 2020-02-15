4ae2b66ce49ac8224a99d48c1d9263b504ac78f1
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-02-15 12:39:30 +0100

    manual dockerization of spring boot

---
+++ manual-dockerization-springboot/src/main/java/com/dockerization/manualdocker/AumateSpringBootRunByDockerApplication.java
@@ -0,0 +1,13 @@
+package com.dockerization.manualdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AumateSpringBootRunByDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AumateSpringBootRunByDockerApplication.class, args);
+	}
+
+}
---
+++ manual-dockerization-springboot/src/test/java/com/dockerization/manualdocker/AumateSpringBootRunByDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerization.manualdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AumateSpringBootRunByDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
